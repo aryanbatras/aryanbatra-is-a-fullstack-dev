386825b902ce0f35e4618752cd66e6ac84075a84
aryanbatras <batraaryan03@gmail.com>
2026-02-18 18:48:43 +0530

    deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,6 @@ export default function Home() {
       <div className={styles.content}>
         <Contact />
       </div>
-      {/* <div className={styles.content}></div> */}
     </div>
   );
 }
